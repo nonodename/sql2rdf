4fed03e654271464477573e88db4a0763047e78f
Dan Bennett <dan_bennett@spglobal.com>
2026-02-17 15:48:36 -0500

    Added serd dependency

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 A C++11 project built with CMake. This project will eventually convert SQL data to RDF.
 
+The repository now includes the [Serd](https://drobilla.net/software/serd/) RDF syntax
+library as a git submodule under `external/serd`. The CMake build system compiles Serd
+from source and links a static `serd` library into the `SQL2RDF++` executable.
+A simple demonstration of calling `serd_strlen` and `serd_strerror` is used in
+`src/main.cpp` to prove the dependency is working.
+
+To build:
+
+```sh
+mkdir build && cd build
+cmake ..
+make
+./SQL2RDF++
+```
+
 ## Building
 
 ```bash
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <serd/serd.h>
 
 int main() {
     std::cout << "Hello SQL2RDF++" << std::endl;
+
+    const char* test = "héllo"; // some utf8 text
+    size_t nbytes = 0;
+    SerdNodeFlags flags = 0;
+    size_t nchars = serd_strlen((const uint8_t*)test, &nbytes, &flags);
+    std::cout << "serd_strlen computed " << nchars << " characters (" << nbytes
+              << " bytes)" << std::endl;
+
+    // also demonstrate strerror helper
+    const uint8_t* msg = serd_strerror(SERD_ERR_BAD_SYNTAX);
+    std::cout << "serd_strerror(SERD_ERR_BAD_SYNTAX) = " << msg << std::endl;
+
     return 0;
 }
